d4a5b3b7ad51470046e0f3f21d50c60764813cf3
pasante1mio-blip <pasante1mio@gmail.com>
2026-08-24 21:25:28 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ⚠️ ÚNICO archivo que debes editar antes de publicar.
 // Pega aquí la URL de tu Web App de Apps Script (termina en /exec).
-const GAS_URL = "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxKl-Et_kRzZpnvMHe8AdYAFVQrVec7XWfAl3-vQh315x0Qq7cf4EgcpynEilR_i9VA/exec";
